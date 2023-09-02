058cf3296ee3a6a831f987e29df35ba1b0a5284f
raidon1 <raidon-77455@hotmail.com>
2023-09-01 23:27:25 -0300

    retornando verbos http post put delet

--- UsandoVerboPost/rest-with-spring-boot-java/src/main/java/br/com/java/PersonController.java
+++ UsandoVerboPost/rest-with-spring-boot-java/src/main/java/br/com/java/PersonController.java
@@ -14,32 +14,46 @@ import br.com.java.model.Person;
 import br.com.java.services.PersonServices;
 
 
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
 	
 	@Autowired
 	private PersonServices service;
-	//private PersonServices service = new PersonServices();
-	 
-    
-	@RequestMapping( method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll()  {
-	   
-	    return service.findAll() ;  
+	///private PersonServices service = new PersonServices();
+	
+	@RequestMapping(method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return service.findAll();
 	}
 	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value =  "id") String id){
-		
-		return service.findByID(id) ;  
+	@RequestMapping(value = "/{id}",
+			method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") String id) {
+		return service.findById(id);
 	}
 	
-	@RequestMapping( method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
-	produces = MediaType.APPLICATION_JSON_VALUE) 
-	public Person create(@RequestBody  Person person){
-		return service.create(person);  
+	@RequestMapping(method=RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {
+		return service.create(person);
 	}
-
-      
+	
+	@RequestMapping(method=RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+		return service.update(person);
+	}
+	
+	
+	@RequestMapping(value = "/{id}",
+			method=RequestMethod.DELETE)
+	public void delete(@PathVariable(value = "id") String id) {
+		service.delete(id);
+	}
 }
--- UsandoVerboPost/rest-with-spring-boot-java/src/main/java/br/com/java/services/PersonServices.java
+++ UsandoVerboPost/rest-with-spring-boot-java/src/main/java/br/com/java/services/PersonServices.java
@@ -16,60 +16,59 @@ public class PersonServices {
 	private final AtomicLong counter = new AtomicLong();
 	
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
-	
 
 	public List<Person> findAll() {
-		
-		logger.info("finding all people!");
+
+		logger.info("Finding all people!");
 		
 		List<Person> persons = new ArrayList<>();
 		for (int i = 0; i < 8; i++) {
 			Person person = mockPerson(i);
 			persons.add(person);
-			
 		}
-			
-		
 		return persons;
 	}
 
-
+	public Person findById(String id) {
 		
-	
-	public Person findByID(String id) {
+		logger.info("Finding one person!");
 		
-		logger.info("finding one person!");
 		Person person = new Person();
 		person.setId(counter.incrementAndGet());
-		person.setFirstName("Raidon");
-		person.setLastName("Rodrigues");
-		person.setAddress("Alto Alegre do Maranhão- Maranhão- Brasil");
-		person.setGender("Male");		 
-		return person;
-		
-	} 
-	public Person create(Person person) {
-		logger.info("creating one person!");
-		
-		return person;
-		
-	} 
-	public Person update(Person person) {
-		logger.info("creating one person!");
-		
-		return person;
-		
-	} 
-	
-	private Person mockPerson(int i) {
-	
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Person name " + i );
-		person.setLastName("Last name "  + i);
-		person.setAddress("Some addres in Brasil " + i);
+		person.setFirstName("Leandro");
+		person.setLastName("Costa");
+		person.setAddress("Uberlândia - Minas Gerais - Brasil");
 		person.setGender("Male");
 		return person;
 	}
+	
+	public Person create(Person person) {
 
+		logger.info("Creating one person!");
+		
+		return person;
+	}
+	
+	public Person update(Person person) {
+		
+		logger.info("Updating one person!");
+		
+		return person;
+	}
+	
+	public void delete(String id) {
+		
+		logger.info("Deleting one person!");
+	}
+	
+	private Person mockPerson(int i) {
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some address in Brasil " + i);
+		person.setGender("Male");
+		return person;
+	}
 }
